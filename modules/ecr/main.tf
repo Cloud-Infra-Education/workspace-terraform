4c4fb23aa4b18d1babed0b0dd4496b6900511683
root <root@maxjagger.localdomain>
2026-01-01 16:10:50 +0900

    Feat: 개인표기용 주석 복구후 재업로드

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,19 +1,16 @@
 resource "aws_ecr_repository" "user" {
   provider             = aws.seoul
-#  name                 = "user-service"
-  name                 = "chan-user-service"
+  name                 = "user-service"
   image_tag_mutability = "IMMUTABLE"
 }
 resource "aws_ecr_repository" "order" {
   provider             = aws.seoul
-#  name                 = "order-service"
-  name                 = "chan-order-service"
+  name                 = "order-service"
   image_tag_mutability = "IMMUTABLE"
 }
 resource "aws_ecr_repository" "product" {
   provider             = aws.seoul
-#  name                 = "product-service"
-  name                 = "chan-product-service"
+  name                 = "product-service"
   image_tag_mutability = "IMMUTABLE"
 }
 
@@ -22,19 +19,16 @@ resource "aws_ecr_repository" "product" {
 
 resource "aws_ecr_repository" "user_oregon" {
   provider             = aws.oregon
-#  name                 = "user-service"
-  name                 = "chan-user-service"
+  name                 = "user-service"
   image_tag_mutability = "IMMUTABLE"
 }
 resource "aws_ecr_repository" "order_oregon" {
   provider             = aws.oregon
-#  name                 = "order-service"
-  name                 = "chan-order-service"
+  name                 = "order-service"
   image_tag_mutability = "IMMUTABLE"
 }
 resource "aws_ecr_repository" "product_oregon" {
   provider             = aws.oregon
-#  name                 = "product-service"
-  name                 = "chan-product-service"
+  name                 = "product-service"
   image_tag_mutability = "IMMUTABLE"
 }
