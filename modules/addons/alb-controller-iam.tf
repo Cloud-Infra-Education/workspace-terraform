dc5a0d096fc708fefe5ce609aa8b981f8aba69cf
root <root@maxjagger.localdomain>
2026-01-01 13:38:16 +0900

    Feat: 개인표기 주석 복구후 재업로드

--- modules/addons/alb-controller-iam.tf
+++ modules/addons/alb-controller-iam.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_policy" "alb_controller" {
-#  name = "AWSLoadBalancerControllerIAMPolicy"
-  name = "chan-AWSLoadBalancerControllerIAMPolicy"
+  name = "AWSLoadBalancerControllerIAMPolicy"
 
   policy = file("${path.module}/iam_policy.json")
 }
